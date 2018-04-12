c660c2e0a736b398cbcfe46b2141d21a0cb44547
Larry Lai <dppss91102@gmail.com>
2018-04-12 23:53:16 +0800

    Linked list structure changed. (Not Accepted: 5/10)

--- main.cpp
+++ main.cpp
@@ -8,44 +8,51 @@ private:
         int price;
         string gift;
         Node* next;
-        Node (int p = 0, string g = "\0", Node* n = nullptr){
+        Node (int p = 0, string g = "", Node* n = nullptr){
             price = p;
             gift = g;
             next = n;
         };
     };
-    Node* first;
+    class Head{
+    public:
+        Node* first;
+    };
+    Head head;
     Node* last;
     Node* search(int price, bool previous) {
-        Node* now = first->next;
-        Node* temp = first;
+        Node* now = head.first;
+        Node* temp = head.first;
         while (now != nullptr) {
             if (now->price == price)
                 return previous ? temp : now;
             else {
                 temp = now;
                 now = now->next;
-                if (now == nullptr)
-                    break;
             }
         }
-
         return nullptr;
     };
 
 public:
     List() {
-        first = new Node();
-        last = first;
+        head.first = nullptr;
+        last = nullptr;
     };
     void insertBack(int price, string gift);
-    void insertAfter(int price, string gift, int prev);
+    void insertAfter(int price, string gift, int place);
     void Delete(int price);
     void Reverse();
     void print ();
     bool empty();
 };
 void List::insertBack(int price, string gift) {
+    if (empty()) {
+        head.first = new Node(price, gift, nullptr);
+        last = head.first;
+        //print();
+        return;
+    }
     if (search(price, false) != nullptr)
         return;
     Node *temp = new Node(price, gift, nullptr);
@@ -53,55 +60,64 @@ void List::insertBack(int price, string gift) {
     last = temp;
     //print();
 }
-void List::insertAfter(int price, string gift, int prev) {
-    if (price == prev)
+void List::insertAfter(int price, string gift, int place) {
+    if (empty())
         return;
-    Node* currentNode = search(prev, false);
+    Node* currentNode = search(place, false);
     if (currentNode != nullptr) {
-        Node* newNode = new Node(price, gift, currentNode->next);
-        currentNode->next = newNode;
+        Node* temp = new Node(price, gift, currentNode->next);
+        currentNode->next = temp;
+        if (currentNode == last)
+            last = temp;
     }
     //print();
 }
 void List::Delete(int price) {
+    if (empty())
+        return;
     Node* prevNode = search(price, true);
     if (prevNode != nullptr) {
+        if (prevNode == last && prevNode == head.first) {
+            delete head.first;
+            head.first = nullptr;
+            last = nullptr;
+            return;
+        }
         if (prevNode->next == last)
-            last = first;
+            last = prevNode;
         Node* temp = prevNode->next->next;
-        prevNode->next = nullptr;
         delete prevNode->next;
         prevNode->next = temp;
     }
     //print();
 }
 void List::Reverse() {
-    Node *current = first->next, *prev = nullptr, *temp = nullptr;
-    last = (current == nullptr) ? first : current;
+    if (empty())
+        return;
+    Node *current = head.first, *prev = nullptr, *temp = nullptr;
+    last = current;
     while (current != nullptr){
         temp = prev;
         prev = current;
         current = current->next;
         prev->next = temp;
     }
-    first->next = prev;
+    head.first = prev;
     //print();
 }
 void List::print() {
-    Node* now = first->next;
+    Node* now = head.first;
     while (now != nullptr) {
         cout << '(' << now->gift << ',' << now->price << ')';
         now = now->next;
-        if (now == nullptr) {
+        if (now == nullptr)
             cout << endl;
-            return;
-        } else {
+        else
             cout << "->";
-        }
     }
 }
 bool List::empty() {
-    return first == last;
+    return head.first == nullptr;
 }
 
 int main() {
@@ -119,9 +135,9 @@ int main() {
         } else if (command == "InsertAfter") {
             cin >> gift;
             cin >> price;
-            int prev;
-            cin >> prev;
-            gift_list.insertAfter(price, gift, prev);
+            int place;
+            cin >> place;
+            gift_list.insertAfter(price, gift, place);
         } else if (command == "Delete") {
             cin >> price;
             gift_list.Delete(price);
@@ -136,8 +152,6 @@ int main() {
             }
             return 0;
         } else
-            break;
+            return 0;
     }
-
-    return 0;
 }
